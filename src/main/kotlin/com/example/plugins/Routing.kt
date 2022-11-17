09517cc69932ffcc3dd374b30efe751a1d942d17
chandrakanth <Bodagala.chandrakanth@japptech.com>
2022-11-17 12:03:14 +0530

    added CORS to test in local host

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -6,7 +6,9 @@ import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 
@@ -14,6 +16,16 @@ fun Application.configureRouting() {
     install(ContentNegotiation) {
         json()
     }
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader("MyCustomHeader")
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
 
     routing {
        /* get("/") {
